3d19e64dd0a2a9d00c9b481c1e357ba04559e1a1
BrandonLeon05 <56412313+BrandonLeon05@users.noreply.github.com>
2021-08-04 18:19:14 -0500

    Implementacion de sidebar y modificaciones en el footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, 
-  { path: 'notFound', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) }];
+  { path: 'notFound', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) },
+  { path: 'departamento', loadChildren: () => import('./pages/departamento/departamento.module').then(m => m.DepartamentoModule) },
+  { path: 'login', loadChildren: () => import('./pages/auth/login/login.module').then(m => m.LoginModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/material.modules.ts
+++ src/app/material.modules.ts
@@ -1,10 +1,18 @@
 import { NgModule } from "@angular/core";
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
-const myModules = [MatButtonModule, MatCardModule, MatToolbarModule, MatIconModule];
+const myModules = [MatButtonModule, 
+                   MatCardModule, 
+                   MatToolbarModule, 
+                   MatIconModule, 
+                   MatSidenavModule,
+                   MatListModule
+                ];
 
 @NgModule({
     imports: [...myModules],
