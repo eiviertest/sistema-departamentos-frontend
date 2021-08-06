a5942b210bf0d12546456dbb5841cbfa9e6f81ed
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2021-08-05 20:13:30 -0500

    Delete guard a Departamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,13 @@ import { CheckLoginGuard } from '@shared/guards/check-login.guard';
 const routes: Routes = [
   { 
     path: '',
-    loadChildren: () => import('./pages/departamento/departamento.module').then(m => m.DepartamentoModule), 
-    canActivate: [CheckLoginGuard] 
+    loadChildren: () => import('./pages/departamento/departamento.module').then(m => m.DepartamentoModule) 
   }, 
   { path: 'notFound',
     loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) 
   },
   { path: 'departamento',
-    loadChildren: () => import('./pages/departamento/departamento.module').then(m => m.DepartamentoModule), 
-    canActivate: [CheckLoginGuard] 
+    loadChildren: () => import('./pages/departamento/departamento.module').then(m => m.DepartamentoModule) 
   },
   { path: 'login',
     loadChildren: () => import('./pages/auth/login/login.module').then(m => m.LoginModule), 
